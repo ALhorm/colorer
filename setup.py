7d893b3d80b0027862bb14003e84822485c60e84
ALhorm <75273270+ALhorm@users.noreply.github.com>
2023-01-06 18:12:38 +0300

    root

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='colorer',
+    version='1.0.0',
+    author='ALhorm',
+    author_email='gladkoam@gmail.com',
+    description='A simple and handy library for coloring strings.',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.0'
+)
