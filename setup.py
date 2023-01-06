90e299286ffdfb4d37150f65fd0df4a5b15538aa
ALhorm <75273270+ALhorm@users.noreply.github.com>
2023-01-06 21:57:48 +0300

    edited setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=readme(),
     long_description_content_type='text/markdown',
     url='https://github.com/ALhorm/colorer',
-    packages=find_packages(),
+    packages=['colorer'],
     classifiers=[
         'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
